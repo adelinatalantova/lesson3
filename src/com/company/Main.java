e34e61548f70b0a14ed40a8be4f7cc47cddcfacc
Admin <adelatalantova23@gmail.com>
2021-08-20 21:24:24 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("        *******");
+        System.out.println("       *********");
+        System.out.println("      ***      ***");
+        System.out.println("     ***        ***");
+        System.out.println("    ***          ***");
+        System.out.println("   ******************");
+        System.out.println("   ******************");
+        System.out.println("   ***            ***");
+        System.out.println("   ***            ***");
+        System.out.println("   ***            ***");
+
+        System.out.println("_________________________________________");
+
+
+        System.out.println("     ******************" );
+        System.out.println("     ******************");
+        System.out.println("     ***            ***");
+        System.out.println("     ***            ***");
+        System.out.println("     ***            ***");
+        System.out.println("  *************************");
+        System.out.println("  *************************");
+        System.out.println("  ******             ******");
+        System.out.println("  ******             ******");
+        System.out.println("  ******             ******");
+
+        System.out.println("_________________________________________");
+
+
+        System.out.println("  ************************** ");
+        System.out.println("  **************************");
+        System.out.println("  ****");
+        System.out.println("  ****");
+        System.out.println("  ****");
+        System.out.println("  **************************");
+        System.out.println("  **************************");
+        System.out.println("  ****");
+        System.out.println("  ****  ");
+        System.out.println("  ****");
+        System.out.println("  **************************");
+        System.out.println("  **************************");
+
+
+    }
+
+}
